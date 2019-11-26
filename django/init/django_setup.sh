adc4b17e22309d894cf48b05470707cee35aa294
oupirum <oupirum@gmail.com>
2019-11-27 01:19:08 +0300

    Create django superuser on startup phase

--- django/init/django_setup.sh
+++ django/init/django_setup.sh
@@ -4,3 +4,13 @@ trap 'exit $?' ERR
 
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth.models import User;
+
+if not User.objects.filter(username='$DJANGO_SU_USER').exists():
+    User.objects.create_superuser('$DJANGO_SU_USER', '$DJANGO_SU_EMAIL', '$DJANGO_SU_PASSWORD');
+    print('Superuser created');
+else:
+    print('Superuser already exist');
+EOF
