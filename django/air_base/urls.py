8683bbbd96396ddb4807f7f6d54243a6ddaad4df
oupirum <oupirum@gmail.com>
2020-03-01 10:55:13 +0300

    Fix babel decorators settings

--- django/air_base/urls.py
+++ django/air_base/urls.py
@@ -5,8 +5,8 @@ from air_base.views.index import IndexView
 # from air_base.views.api.air_base import DepartmentView, EmployeeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
+	path('admin/', admin.site.urls),
+	path('', IndexView.as_view(), name='index'),
 
 	# path('api/department/<str:id>', DepartmentView.as_view()),
 	# path('api/employee/<str:id>', EmployeeView.as_view()),
